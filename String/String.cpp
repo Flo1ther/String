3313f84490ca2cc6dca0936a045b2dc37bca4765
Myroslav <shevchuk.nirosla@gmail.com>
2024-03-27 22:22:04 +0200

    Second commit

--- String/String.cpp
+++ String/String.cpp
@@ -2,47 +2,48 @@
 
 #include <iostream>
 #include <cstring>
+
+template <class T>
 class String
 {
 public:
-	String() : text(nullptr) {}
-	String(const char* line) : size{ int(strlen(line) + 1) }, text{ new char[size] }
+	String() : text(nullptr), size(0), capacity(0), pointer(nullptr) {}
+
+	String(const char* line) : size(static_cast<int>(strlen(line) + 1)), text(new char[size]), capacity(size), pointer(new T[size])
 	{
 		for (int i = 0; i < size; i++)
 		{
 			this->text[i] = line[i];
 		}
 	}
-	String(const String& copy)
+
+	String(const String& copy) : size(copy.size), capacity(copy.capacity), text(new char[copy.size]), pointer(new T[copy.size])
 	{
 		if (copy.text)
 		{
-			size = copy.size;
-			text = new char[size];
 			for (int i = 0; i < size; i++)
 			{
 				this->text[i] = copy.text[i];
 			}
 		}
-		else
-		{
-			text = nullptr;
-			size = 0;
-		}
 	}
 
 	~String()
 	{
-		delete[]text;
+		delete[] text;
+		delete[] pointer;
 	}
+
 	int getSize()
 	{
 		return size;
 	}
+
 	char* getText()
 	{
 		return text;
 	}
+
 	void setText(const char* text_)
 	{
 		for (int i = 0; i < size; i++)
@@ -50,16 +51,18 @@ public:
 			this->text[i] = text_[i];
 		}
 	}
+
 	void setSize(const int size_)
 	{
 		this->size = size_;
 	}
-	void Changeline(const char* line)
+
+	void ChangeLine(const char* line)
 	{
-		
 		if (line)
 		{
-			size = int(strlen(line) + 1);
+			size = static_cast<int>(strlen(line) + 1);
+			delete[] text;
 			text = new char[size];
 			for (int i = 0; i < size; i++)
 			{
@@ -68,19 +71,21 @@ public:
 		}
 		else
 		{
+			delete[] text;
 			text = nullptr;
 			size = 0;
 		}
 	}
-	String returnline(int start, int end) 
+
+	String returnLine(int start, int end)
 	{
-		if (text&&start>=0&&end<strlen(text)&&start<=end)
+		if (text && start >= 0 && end < size && start <= end)
 		{
 			char* ret = new char[end - start + 2];
 			strncpy(ret, text + start, end - start + 1);
 			ret[end - start + 1] = '\0';
 			String result(ret);
-			delete[]ret;
+			delete[] ret;
 			return result;
 		}
 		else
@@ -88,16 +93,35 @@ public:
 			return String();
 		}
 	}
-	void ShowString()
-	const
+
+	void ShowString() const
 	{
 		if (text)
 		{
 			std::cout << text;
 		}
 	}
-	int countWords(const char* word)
-		const
+
+	void pop_back()
+	{
+		if (size > 0)
+		{
+			size--;
+			if (size < capacity - 4)
+			{
+				T* tmp = new T[capacity - 4];
+				for (int i = 0; i < size; i++)
+				{
+					tmp[i] = pointer[i];
+				}
+				delete[] pointer;
+				pointer = tmp;
+				capacity -= 4;
+			}
+		}
+	}
+
+	int countWords(const char* word) const
 	{
 		if (!text || !word)
 		{
@@ -112,32 +136,37 @@ public:
 			{
 				count++;
 			}
-			token = strtok(nullptr, "");
+			token = strtok(nullptr, " ");
 		}
 		return count;
 	}
-	
+
 private:
 	char* text;
 	int size;
+	int capacity;
+	T* pointer;
 };
+
 int main()
 {
-	String emptyString;
-	String stringParam("Hello world!");
-	String copiedString = stringParam;
+	String<int> emptyString;
+	String<int> stringParam("Hello world!");
+	String<int> copiedString = stringParam;
 	emptyString.ShowString();
 	std::cout << std::endl;
 	stringParam.ShowString();
 	std::cout << std::endl;
 	copiedString.ShowString();
 	std::cout << std::endl;
-	copiedString.Changeline("New string");
+	copiedString.ChangeLine("New string");
 	copiedString.ShowString();
 	std::cout << std::endl;
-	String returnLineResult = copiedString.returnline(1, 7);
+	String<int> returnLineResult = copiedString.returnLine(1, 7);
 	returnLineResult.ShowString();
 	std::cout << std::endl;
 	int wordCount = copiedString.countWords("New");
 	std::cout << "Word count: " << wordCount << std::endl;
+
+	return 0;
 }
